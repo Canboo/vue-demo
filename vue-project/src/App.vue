660fa59f2ab7a1171fd1a8c050defc6e7197a3de
Tsai Cheng Peng <Canboo@users.noreply.github.com>
2025-07-18 14:02:16 +0800

    嘗試建立第一個component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import DialogComp from './components/DialogComp.vue'
 </script>
 
 <template>
@@ -15,31 +16,8 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
       </nav>
 
-      <v-dialog max-width="500">
-        <template v-slot:activator="{ props: activatorProps }">
-          <v-btn
-            v-bind="activatorProps"
-            color="surface-variant"
-            text="Open Dialog"
-            variant="flat"
-          ></v-btn>
-        </template>
-
-        <template v-slot:default="{ isActive }">
-          <v-card title="Dialog">
-            <v-card-text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua.
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-
-              <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
-            </v-card-actions>
-          </v-card>
-        </template>
-      </v-dialog>
+      <DialogComp btnText="第一個按鈕" />
+      <DialogComp btnText="第二個按鈕" />
     </div>
   </header>
 
---
+++ vue-project/src/components/DialogComp.vue
@@ -0,0 +1,31 @@
+<script setup>
+defineProps({
+  btnText: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <v-dialog max-width="500" persistent>
+    <template v-slot:activator="{ props: activatorProps }">
+      <v-btn v-bind="activatorProps" color="surface-variant" :text="btnText" variant="flat"></v-btn>
+    </template>
+
+    <template v-slot:default="{ isActive }">
+      <v-card :title="btnText">
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua.
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
