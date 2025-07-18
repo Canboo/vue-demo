32c2145dda7d1dcfcd9c95d54668f0bc698ee3b7
Tsai Cheng Peng <Canboo@users.noreply.github.com>
2025-07-18 13:24:15 +0800

    npm i vuetify

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -14,6 +14,32 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
+
+      <v-dialog max-width="500">
+        <template v-slot:activator="{ props: activatorProps }">
+          <v-btn
+            v-bind="activatorProps"
+            color="surface-variant"
+            text="Open Dialog"
+            variant="flat"
+          ></v-btn>
+        </template>
+
+        <template v-slot:default="{ isActive }">
+          <v-card title="Dialog">
+            <v-card-text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua.
+            </v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <v-btn text="Close Dialog" @click="isActive.value = false"></v-btn>
+            </v-card-actions>
+          </v-card>
+        </template>
+      </v-dialog>
     </div>
   </header>
 
@@ -21,6 +47,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
+.bg {
+  height: 100px;
+}
 header {
   line-height: 1.5;
   max-height: 100vh;
